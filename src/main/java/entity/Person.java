3bc3135746bf32f5c453efc2bd7903ffbbf257d0
mehdi <mehdiarghan@gmail.com.git/>
2023-05-25 22:36:56 +0330

    _ add class person into package entity

---
+++ src/main/java/entity/Person.java
@@ -0,0 +1,52 @@
+package entity;
+
+public class Person {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private Long password;
+
+    public Person() {
+    }
+
+    public Person(int id, String firstName, String lastName, Long password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Long getPassword() {
+        return password;
+    }
+
+    public void setPassword(Long password) {
+        this.password = password;
+    }
+}
+
+
