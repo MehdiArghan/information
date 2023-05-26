cf2cd0e848872177f19f28cecf56eaf6e6dfcb5a
mehdi <mehdiarghan@gmail.com.git/>
2023-05-26 16:12:01 +0330

    - merge child into master

--- src/main/java/entity/Main.java
+++ src/main/java/entity/Main.java
@@ -5,6 +5,7 @@ import database.Metod;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class Main {
     public static void main(String[] args) {
        /* Users users = new Users(2, "sorosh", "hichkas", 20);
